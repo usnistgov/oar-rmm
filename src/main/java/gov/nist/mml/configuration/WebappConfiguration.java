c39e889e5bbb8c839d159b183e17034a8b7d682f
deoyani <deoyani.pics@gmail.com>
2016-08-05 13:14:42 -0400

    CORS enabled.

--- src/main/java/gov/nist/mml/configuration/WebappConfiguration.java
+++ src/main/java/gov/nist/mml/configuration/WebappConfiguration.java
@@ -19,6 +19,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -43,4 +44,11 @@ public class WebappConfiguration extends WebMvcConfigurerAdapter{
 	    registry.addResourceHandler("/webjars/**")
 	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
+	
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+	
 }
