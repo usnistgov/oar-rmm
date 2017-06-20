c5b0346a403bef965f998d2641da5477386ecba9
Harold Affo <harold.affo@nist.gov>
2017-06-20 16:40:04 -0400

    Create beforecicd.sh

---
+++ scripts/dev/beforecicd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo rm -r /opt/data/backup/oar-rmm-service/
+if [ -f /home/ubuntu/oar-docker/rmm/restapi/app/oar-rmm-service.jar ];
+then
+  #backup previous build
+  sudo cp -r /home/ubuntu/oar-docker/rmm/restapi/app/oar-rmm-service.jar /opt/data/backup/oar-rmm-service/
+  #remove previous build
+  sudo rm -r /home/ubuntu/oar-docker/rmm/restapi/app/oar-rmm-service.jar
+fi
