0c72c49b5a933c0ca0277e87df5e9125848360a1
Harold Affo <harold.affo@nist.gov>
2017-06-27 13:18:48 -0400

    Update aftercicd.sh

--- scripts/dev/aftercicd.sh
+++ scripts/dev/aftercicd.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-cd /home/ubuntu/oar-docker/apps/
-
-if [[ $(sudo docker ps -aqf "name=rmm") ]]; then
-    sudo docker rm -f $(sudo docker ps -aqf "name=rmm")
-fi
-if [[ $(sudo docker images rmm -aq) ]]; then
-   sudo docker rmi -f $(sudo docker images rmm -aq)
-fi
-
-sudo docker-compose rm -f
-sudo docker-compose build --no-cache
-sudo docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+sudo /home/ubuntu/oar-docker/apps/rmm/clear.sh 
+sudo /home/ubuntu/oar-docker/apps/deployAll-dev.sh
