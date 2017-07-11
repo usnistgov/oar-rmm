3f0b95addc7bba90a1bf11adbb3bcda923b7e9fc
deoyani <deoyani.pics@gmail.com>
2017-07-11 15:44:43 -0400

    Changes to use config server

--- src/main/java/gov/nist/oar/rmm/config/MongoConfig.java
+++ src/main/java/gov/nist/oar/rmm/config/MongoConfig.java
@@ -23,6 +23,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -35,6 +36,7 @@ import com.mongodb.client.MongoDatabase;
 
 @Configuration
 @ConfigurationProperties	
+@EnableAutoConfiguration
 /**
  * MongoDB configuration, reading all the conf details from 
  * application.yml
