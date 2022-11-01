47f7bc8798ee173dafb100a85c669f95e8a9b69d
Deoyani Heinis <deoyani.pics@gmail.com>
2022-11-01 12:47:08 -0400

    Updated the tests to make sure changes in versions related files are updated and tested

--- src/test/java/gov/nist/oar/rmm/unit/repositories/impl/VersionsReleasesetsRepositoryImplTest.java
+++ src/test/java/gov/nist/oar/rmm/unit/repositories/impl/VersionsReleasesetsRepositoryImplTest.java
@@ -17,7 +17,7 @@ import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.bson.Document;
@@ -25,111 +25,124 @@ import org.bson.conversions.Bson;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import com.github.fakemongo.junit.FongoRule;
-import com.mongodb.AggregationOutput;
 import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-import com.mongodb.util.FongoJSON;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.ImmutableMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfig;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
 import gov.nist.oar.rmm.repositories.VersionReleasesetsRepository;
 import gov.nist.oar.rmm.utilities.ProcessRequest;
-
-@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
+@TestInstance(Lifecycle.PER_CLASS)
+@TestPropertySource("classpath:bootstrap-test.yml")
 public class VersionsReleasesetsRepositoryImplTest implements VersionReleasesetsRepository {
 
-    private Logger logger = LoggerFactory.getLogger(CustomRepositoryImplTest.class);
-    @Rule
-    public FongoRule fongoRule = new FongoRule();
-    DBCollection versionsCollection, releasesetsCollection;
+	
+	private static final String CONNECTION_STRING = "mongodb://%s:%d";
 
-    @Before
-    public void initIt() throws Exception {
+	private Logger logger = LoggerFactory
+			.getLogger(VersionsReleasesetsRepositoryImplTest.class);
+	private MongodExecutable mongodExecutable;
+	private MongoTemplate mongoTemplate;
 
-        versionsCollection = fongoRule.getDB("TestDBtemp").getCollection("versionstest");
-        JSONParser parser = new JSONParser();
-        JSONArray a;
-        File file = new File(this.getClass().getClassLoader().getResource("versions.json").getFile());
-        try {
-            a = (JSONArray) parser.parse(new FileReader(file));
-            for (Object o : a) {
-                DBObject dbObject = (DBObject) com.mongodb.util.JSON.parse(o.toString());
-                versionsCollection.save(dbObject);
-            }
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
+	@AfterAll
+	void clean() {
+		mongodExecutable.stop();
+	}
 
-        /// Taxonomy collection;
-        releasesetsCollection = fongoRule.getDB("TestDBtemp").getCollection("releasesets");
-        parser = new JSONParser();
+	@BeforeAll
+	void setup() throws Exception {
+		String ip = "localhost";
+		int port = 21432;
 
-        file = new File(this.getClass().getClassLoader().getResource("releaseSets.json").getFile());
-        try {
-            a = (JSONArray) parser.parse(new FileReader(file));
-            for (Object o : a) {
-                DBObject dbObject = (DBObject) com.mongodb.util.JSON.parse(o.toString());
-                releasesetsCollection.save(dbObject);
-            }
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
-    }
+		ImmutableMongodConfig mongodConfig = MongodConfig.builder()
+				.version(Version.Main.PRODUCTION)
+				.net(new Net(ip, port, Network.localhostIsIPv6())).build();
 
-    //// Functions to help test
-    private DBObject dbObject(Bson bson) {
-        if (bson == null) {
-            return null;
-        }
+		MongodStarter starter = MongodStarter.getDefaultInstance();
+		mongodExecutable = starter.prepare(mongodConfig);
+		mongodExecutable.start();
+		mongoTemplate = new MongoTemplate(
+				MongoClients.create(String.format(CONNECTION_STRING, ip, port)),
+				"test");
 
-        // TODO Performance killer
-        return (DBObject) FongoJSON
-                .parse(bson.toBsonDocument(Document.class, MongoClient.getDefaultCodecRegistry()).toString());
-    }
+		JSONParser parser = new JSONParser();
+		JSONArray a;
+		File file = new File(this.getClass().getClassLoader()
+				.getResource("versions.json").getFile());
 
-    @Override
+		try {
+			a = (JSONArray) parser.parse(new FileReader(file));
+			for (Object o : a) {
+				Document doc = Document.parse(o.toString());
+				mongoTemplate.save(doc, "versions");
+			}
+		} catch (IOException | ParseException e) {
+
+
+			e.printStackTrace();
+		}
+
+		file = new File(this.getClass().getClassLoader()
+				.getResource("releaseSets.json").getFile());
+		try {
+			a = (JSONArray) parser.parse(new FileReader(file));
+			for (Object o : a) {
+				Document doc = Document.parse(o.toString());
+				mongoTemplate.save(doc, "releaseSet");
+
+			}
+		} catch (IOException | ParseException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+
+    
     public Document findVersion(MultiValueMap<String, String> params, Pageable p) {
 
         ProcessRequest request = new ProcessRequest();
         request.parseSearch(params);
         long count = 0;
         if (request.getFilter() == null)
-            count = versionsCollection.count();
+            count = mongoTemplate.getCollection("versions").countDocuments();
         else {
             Bson b = request.getFilter();
-            count = versionsCollection.count((BasicDBObject) dbObject(b));
+            count =  mongoTemplate.getCollection("versions").countDocuments(b);
         }
 
         logger.info("Count :" + count);
-        Document resultDoc = new Document();
-        resultDoc.put("Versions Count", count);
-        resultDoc.put("PageSize", request.getPageSize());
+    
         List<Bson> dList = request.getQueryList();
-        List<BasicDBObject> dobList = new ArrayList<BasicDBObject>();
-        int i = 0;
-        while (dList.size() > i) {
-            dobList.add((BasicDBObject) dbObject(dList.get(i)));
-            i++;
-        }
-        AggregationOutput ag = versionsCollection.aggregate(dobList);
-        List<DBObject> dlist = new ArrayList<DBObject>();
-        for (DBObject dbObject : ag.results()) {
-            dlist.add(dbObject);
-        }
-        resultDoc.put("Versions data", dlist);
+
+        AggregateIterable<Document> ag =  mongoTemplate.getCollection("versions").aggregate(dList);        
+        Document resultDoc = new Document();
+        resultDoc.put("Version" + "Count", count);
+        resultDoc.put("PageSize", request.getPageSize());
+        resultDoc.put("Version" + "Data", ag);
         return resultDoc;
     }
 
@@ -137,14 +150,9 @@ public class VersionsReleasesetsRepositoryImplTest implements VersionReleasesets
     public void testFindVersions() {
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-
         Document r = findVersion(params, null);
         long resCnt = 15;
-        List<DBObject> rdata = (List<DBObject>) r.get("Versions data");
-        for (DBObject rd : rdata) {
-            // System.out.println(rd.get("title"));
-        }
-        assertEquals(r.get("Versions Count"), resCnt);
+        assertEquals(r.get("VersionCount"), resCnt);
     }
 
     @Test
@@ -153,12 +161,15 @@ public class VersionsReleasesetsRepositoryImplTest implements VersionReleasesets
         MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
         params.add("@id", "ark:/88434/mds2-2106.rel");
         Document r1 = findVersion(params, null);
-        List<DBObject> rdata1 = (List<DBObject>) r1.get("Versions data");
+        AggregateIterable<Document> aggregateIterable =  (AggregateIterable<Document>) r1.get("VersionData");
+        Iterator iterator = aggregateIterable.iterator();
         String title = "";
-
-        for (DBObject rd : rdata1) {
-            title = rd.get("title").toString();
+        while (iterator.hasNext()) {
+            Document d =(Document) iterator.next();
+            title = d.getString("title");
+            break;
         }
+
         assertEquals(
                 "Data from: Collaborative Guarded-Hot-Plate Tests between the National Institute of Standards and Technology and the National Physical Laboratory",
                 title);
@@ -168,19 +179,9 @@ public class VersionsReleasesetsRepositoryImplTest implements VersionReleasesets
     @Test
     public void testReleasesets() {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-
         Document r = findReleaseset(params, null);
-        long resCnt = 4;
-        long resCntdb = 0;
-        String titleReleaseSet = "";
-        List<DBObject> rdata = (List<DBObject>) r.get("ReleaseSets data");
-        for (DBObject rd : rdata) {
-//	    System.out.println(rd.get("title")+ " ::  "+r.get("ReleaseSets count"));
-            titleReleaseSet = rd.get("title").toString();
-
-        }
-        assertEquals(r.get("ReleaseSets count"), resCnt);
-
+        long resCnt = 4;   
+        assertEquals(r.get("ReleaseSetsCount"), resCnt);
     }
 
     @Override
@@ -190,30 +191,20 @@ public class VersionsReleasesetsRepositoryImplTest implements VersionReleasesets
 
         long count = 0;
         if (request.getFilter() == null)
-            count = releasesetsCollection.count();
+            count = mongoTemplate.getCollection("releaseSet").countDocuments();
         else {
             Bson b = request.getFilter();
 
-            count = releasesetsCollection.count((BasicDBObject) dbObject(b));
+            count = mongoTemplate.getCollection("releaseSet").countDocuments(b);
         }
 
-        logger.info("ReleaseSets Count :" + count);
+        logger.info("ReleaseSetsCount :" + count);
         Document resultDoc = new Document();
-        resultDoc.put("ReleaseSets count", count);
+        resultDoc.put("ReleaseSetsCount", count);
         resultDoc.put("PageSize", request.getPageSize());
         List<Bson> dList = request.getQueryList();
-        List<BasicDBObject> dobList = new ArrayList<BasicDBObject>();
-        int i = 0;
-        while (dList.size() > i) {
-            dobList.add((BasicDBObject) dbObject(dList.get(i)));
-            i++;
-        }
-        AggregationOutput ag = releasesetsCollection.aggregate(dobList);
-        List<DBObject> dlist = new ArrayList<DBObject>();
-        for (DBObject dbObject : ag.results()) {
-            dlist.add(dbObject);
-        }
-        resultDoc.put("ReleaseSets data", dlist);
+        AggregateIterable<Document> ag = mongoTemplate.getCollection("releaseSet").aggregate(dList);
+        resultDoc.put("ReleaseSetsData", ag);
         return resultDoc;
     }
 
@@ -224,39 +215,45 @@ public class VersionsReleasesetsRepositoryImplTest implements VersionReleasesets
 
     @Test
     public void testfindReleaseset() {
-        Document r = findReleaseset("ark:/88434/mds00sxbvh.rel");
-        List<DBObject> rdata = (List<DBObject>) r.get("ReleaseSets data");
-        for (DBObject rd : rdata) {
-            System.out.println(rd.get("title"));
+    	 MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+         params.add("@id", "ark:/88434/mds00sxbvh.rel");
+         Document r = findReleaseset(params, null);
+        
+        AggregateIterable<Document> aggregateIterable =  (AggregateIterable<Document>) r.get("ReleaseSetsData");
+        Iterator iterator = aggregateIterable.iterator();
+        String title = "";
+        while (iterator.hasNext()) {
+            Document d =(Document) iterator.next();
+            title = d.getString("title");
+            break;
         }
-
+        assertEquals("NIST Mugshot Identification Database (MID) - NIST Special Database 18", title);
     }
 
     @Override
     public Document findReleaseset(String id) {
-        return queryResult("ark:/88434/mds00sxbvh.rel", "releaseSets");
+        return queryResult("ark:/88434/mds00sxbvh.rel", "releaseSet");
 
     }
 
     private Document queryResult(String id, String collectionName) {
 
-        DBCollection mcollection = null;
-        if ("versions".equalsIgnoreCase(collectionName))
-            mcollection = versionsCollection;
+        MongoCollection<Document> mcollection = null;
+        if ("version".equalsIgnoreCase(collectionName))
+            mcollection = mongoTemplate.getCollection("version");
 
-        else if ("releaseSets".equalsIgnoreCase(collectionName))
-            mcollection = releasesetsCollection;
-        String useid = id;
+        else if ("releaseSet".equalsIgnoreCase(collectionName))
+            mcollection = mongoTemplate.getCollection("releaseSet");
+
         Document resultDoc = new Document();
         resultDoc.put("ReleaseSets count", 1);
         resultDoc.put("PageSize", 1);
         mcollection.find().limit(1);
-        DBCursor dm = mcollection.find(new BasicDBObject("@id", "ark:/88434/mds00sxbvh.rel"));
-        DBObject resultObject = dm.next();
-
-        List<DBObject> dlist = new ArrayList<DBObject>();
-        dlist.add(resultObject);
-        resultDoc.put("ReleaseSets data", dlist);
+        FindIterable<Document> dm = mcollection.find(new BasicDBObject("@id", "ark:/88434/mds00sxbvh.rel"));
+    
+        resultDoc.put("ReleaseSetData", dm);
         return resultDoc;
     }
+
+
 }
